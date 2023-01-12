11a7109b189570af0d9d2cf678b7e796ff26e8c1
NikitaPas <86974012+NikitaPas@users.noreply.github.com>
2023-01-13 01:12:16 +0300

    feat: added lab10 frontend

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import styles from './Footer.module.css'
+
+
+const Footer = () => {
+    return (
+        <header>
+            <div className="wrapper">
+                <div className={styles.content}>
+                    2023 GitHub: @NikitaPas
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Footer;
